01a62da35bdc9de5d5bbb239f1f0ec8adcb58b8e
ThanhNguyen-work <phuthanh.nguyen.work@gmail.com>
2020-10-13 19:48:24 +0700

    Adding Bernoulli Naive Bayes model

--- Bài tập 01/BaiTap01.py
+++ Bài tập 01/BaiTap01.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn import metrics
 
@@ -24,6 +25,12 @@ def stochasticGradientDescent(X_train, X_test, y_train, y_test):
     y_pred = clf.predict(X_test)
     return metrics.accuracy_score(y_test, y_pred)
 
+def naiveBayesBernoulli(X_train, X_test, y_train, y_test):
+    clf = BernoulliNB()
+    clf = clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    return metrics.accuracy_score(y_test, y_pred)
+
 def main():
     path = '../dataset/spam.csv'
     data = np.genfromtxt(path, delimiter = ',')
@@ -38,6 +45,9 @@ def main():
     # Stochastic Gradient Descent
     sgdAccuracy = stochasticGradientDescent(X_train, X_test, y_train, y_test)
     print(f'Stochastic Gradient Descent Accuracy = {sgdAccuracy}')
+    # Bernoulli Naive Bayes
+    bernoulliAccuracy = naiveBayesBernoulli(X_train, X_test, y_train, y_test)
+    print(f'Bernoulli Naive Bayes Accuracy = {bernoulliAccuracy}')
 
 if __name__ == '__main__':
     main()
