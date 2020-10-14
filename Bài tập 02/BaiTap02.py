db1dc42bbbb648cfc2795ae7334c120906959e02
hduc-lee <hduc.lee@gmail.com>
2020-10-14 22:32:46 +0700

    update BaiTap02.py

--- Bài tập 02/BaiTap02.py
+++ Bài tập 02/BaiTap02.py
@@ -3,19 +3,28 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
-import eli5
-from eli5.sklearn import PermutationImportance
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+# import eli5
+# from eli5.sklearn import PermutationImportance
 
 def decisionTreeModel(X_train,X_test,y_train,y_test):
     clf = DecisionTreeClassifier()
     clf.fit(X_train,y_train)
     y_pred = clf.predict(X_test)
-    perm = PermutationImportance(clf,random_state=1).fit(X_test,y_test)
-    eli5.show_weights(perm,feature_names=X_test.columns.tolist())
     return metrics.accuracy_score(y_pred,y_test)
-
+def supportVectorMachine(X_train,X_test,y_train,y_test):
+    clf = SVC(kernel='poly')
+    clf.fit(X_train,y_train)
+    y_pred = clf.predict(X_test)
+    return metrics.accuracy_score(y_pred,y_test)
+def randomForestModel(X_train,X_test,y_train,y_test):
+    clf = RandomForestClassifier()
+    clf.fit(X_train,y_train)
+    y_pred = clf.predict(X_test)
+    return metrics.accuracy_score(y_pred,y_test)
 def main():
-    data_path = '../diabetes.csv'
+    data_path = '/home/duc-hoang/Documents/Junior-Year/Python-For-Data-Science/Py4DS_Lab1/Py4DS_Lab1_Dataset/diabetes.csv'
     col_names = ['pregnant', 'glucose','bp','skin','isulin','bmi','pedigree','age','label']
     df = pd.read_csv(data_path,header=0, names=col_names)
     # Feature selection
@@ -27,6 +36,13 @@ def main():
     # Build model
     decisionTreeAccuracy = decisionTreeModel(X_train,X_test,y_train,y_test)
     print(f'CART (Tree prediction) Accuracy = {decisionTreeAccuracy}')
+
+    randomForestAccuracy = randomForestModel(X_train,X_test,y_train,y_test)
+    print(f'CART (RF prediction) Accuracy = {randomForestAccuracy}')
+
+    svcAccuracy = supportVectorMachine(X_train,X_test,y_train,y_test)
+    print(f'CART (svm prediction) Accuracy = {svcAccuracy}')
+
 
 if __name__ == '__main__':
     main()
