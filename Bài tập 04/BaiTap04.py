ed599cda0a2abe471a64c616639c91deffb36eda
hduc-lee <hduc.lee@gmail.com>
2020-10-16 17:35:18 +0700

    add LinearRegression and SGD models

--- Bài tập 04/BaiTap04.py
+++ Bài tập 04/BaiTap04.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
 from sklearn import metrics
 from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier, LogisticRegression
 from sklearn.model_selection import train_test_split
 
 def decisionTreeModel(X_train,X_test,y_train,y_test, maxdepth):
@@ -20,6 +21,16 @@ def randomForestModel(X_train,X_test,y_train,y_test):
     return metrics.accuracy_score(y_pred,y_test)
 def supportVectorMachine(X_train,X_test,y_train,y_test):
     mdl = SVC(kernel='poly')
+    mdl.fit(X_train,y_train)
+    y_pred = mdl.predict(X_test)
+    return metrics.accuracy_score(y_pred,y_test)
+def sgdModel(X_train,X_test,y_train,y_test):
+    mdl = SGDClassifier()
+    mdl.fit(X_train,y_train)
+    y_pred = mdl.predict(X_test)
+    return metrics.accuracy_score(y_pred,y_test)
+def LogisticRegression(X_train,X_test,y_train,y_test):
+    mdl = LogisticRegression()
     mdl.fit(X_train,y_train)
     y_pred = mdl.predict(X_test)
     return metrics.accuracy_score(y_pred,y_test)
@@ -42,12 +53,13 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
     # Build model
+
     # Decision Tree model
+
     # compare accuracy with differing values of max_depth
     # for max_depth in [50, 500, 5000, 50000]:
     #     my_mae = decisionTreeModel(X_train, X_test, y_train, y_test,max_depth)
     #     print("Max depth: %d  \t\t Decision Tree accuracy:  %f" %(max_depth, my_mae))
-
     decisionTreeAccuracy = decisionTreeModel(X_train,X_test,y_train,y_test,500) # take max_depth = 500
     print(f'Decision Tree prediction Accuracy = {decisionTreeAccuracy}')
 
@@ -59,6 +71,12 @@ def main():
     svcAccuracy = supportVectorMachine(X_train,X_test,y_train,y_test)
     print(f'SVM prediction Accuracy = {svcAccuracy}')
     
+    # SGD model
+    sgdAccuracy = SGDClassifier(X_train,X_test,y_train,y_test)
+    print(f'SGD prediction Accuracy = {sgdAccuracy}')
 
+    # Linear Regression model
+    lrAccuracy = LogisticRegression(X_train,X_test,y_train,y_test)
+    print(f'SGD prediction Accuracy = {lrAccuracy}')
 if __name__ == '__main__':
     main()
