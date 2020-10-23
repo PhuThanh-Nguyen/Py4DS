ef985218fafebda35dbbf4f9ee060fc995551f11
ThanhNguyen-work <phuthanh.nguyen.work@gmail.com>
2020-10-23 19:46:10 +0700

    Change some comments in file .py

--- Lab 02/Bài tập 04/BaiTap04.py
+++ Lab 02/Bài tập 04/BaiTap04.py
@@ -4,6 +4,7 @@ import seaborn as sns
 
 def main():
 	data = pd.read_csv('../dataset/HappinessReport2020.csv')
+	"""==========================================================================="""
 	'''
 	Nhận xét biểu đồ:
 	+) Số lượng dữ liệu cho khu vực Châu Phi hạ Sahara, trong khi các khu vực ở các khu vực khác khá ít (Khu vực Mỹ Latinh và Caribean)
@@ -11,6 +12,7 @@ def main():
 	'''
 	ax = data['Regional indicator'].value_counts(normalize = True).plot(kind = 'bar')
 	plt.show()
+	"""==========================================================================="""
 	'''
 	Nhận xét biểu đồ:
 	+) Biểu đồ thanh khá đồng đều, tuổi thọ trung bình cao nhất trong khu vực Tây Âu (72.9), thấp nhất trong khu vực Châu Phi (55.5) tuổi
@@ -18,16 +20,16 @@ def main():
 	region = data.groupby('Regional indicator')[['Healthy life expectancy']].agg('mean').reset_index()
 	ax = region.plot(x = 'Regional indicator', y = 'Healthy life expectancy', kind = 'bar')
 	plt.show()
-
+	"""==========================================================================="""
 	'''
 	Nhận xét biểu đồ:
-	+) Các khu vực Tây Âu, khu vực nhóm gồm Bắc Mỹ, New Zealand và Australia có tuổi thọ trung bình cao, không phân tán quá nhiều. Khu 	vực Tây Âu lệch phải trong khi nhóm Bắc Mỹ, New Zealand và Australia lệch trái
+	+) Các khu vực Tây Âu, khu vực nhóm gồm Bắc Mỹ, New Zealand và Australia có tuổi thọ trung bình cao, không phân tán quá nhiều. Khu 		vực Tây Âu lệch phải trong khi nhóm Bắc Mỹ, New Zealand và Australia lệch trái
 	+) Khu vực Đông Á có tuổi thọ trung bình cao, tuy nhiên phân tán mạnh
 	+) Khu vực Sahara hạ Châu Phi có tuổi trung bình thấp, không bị lệch nhiều
 	'''
 	ax = sns.boxplot(x = 'Regional indicator', y = 'Healthy life expectancy', data = data)
 	ax.set_xticklabels(ax.get_xticklabels(), rotation = 90)
-	plt.legend()
 	plt.show()
+	"""==========================================================================="""
 if __name__ == '__main__':
 	main()
