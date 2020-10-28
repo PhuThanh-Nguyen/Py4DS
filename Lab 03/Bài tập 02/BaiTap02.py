4e5ae558e07e542c6fbf62d3ff3b64df47397c75
ThanhNguyen-work <phuthanh.nguyen.work@gmail.com>
2020-10-28 17:48:11 +0700

    Read dataset, drop missing values and drop duplicate values

--- Lab 03/Bài tập 02/BaiTap02.py
+++ Lab 03/Bài tập 02/BaiTap02.py
@@ -1,1 +1,26 @@
-# Add code here
+import pandas as pd
+
+def main():
+	data = pd.read_csv('../dataset/AB_NYC_2019.csv')
+	print('>> Before drop missing values: ')
+	print(f'>> Data shape: {data.shape}')
+	print(data.info())
+	print(data.describe())
+	data = data.dropna()
+	print('>> After drop missing values:')
+	print(f'>> Data shape: {data.shape}')
+	print(data.info())
+	print(data.describe())
+	
+	print('>> Before drop duplicate values: ')
+	print(f'>> Data shape: {data.shape}')
+	print(data.info())
+	print(data.describe())
+	data.drop_duplicates(subset = data.columns.values[:-1], keep = 'first', inplace = True)
+	print('>> After drop duplicate values:')
+	print(f'>> Data shape: {data.shape}')
+	print(data.info())
+	print(data.describe())
+	
+if __name__ == '__main__':
+	main()
