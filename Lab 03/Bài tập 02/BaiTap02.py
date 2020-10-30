c93df89fab7ccbec43b2b169382149a2d7718bc3
ThanhNguyen-work <phuthanh.nguyen.work@gmail.com>
2020-10-30 15:21:18 +0700

    Remove outliers from dataset, add comments

--- Lab 03/Bài tập 02/BaiTap02.py
+++ Lab 03/Bài tập 02/BaiTap02.py
@@ -1,17 +1,24 @@
 import pandas as pd
 
 def main():
+	# Read data
 	data = pd.read_csv('../dataset/AB_NYC_2019.csv')
+	
+	# Remove missing values, duplicates and outliers
+	
+	# Remove missing values:
 	print('>> Before drop missing values: ')
 	print(f'>> Data shape: {data.shape}')
 	print(data.info())
 	print(data.describe())
+	
 	data = data.dropna()
 	print('>> After drop missing values:')
 	print(f'>> Data shape: {data.shape}')
 	print(data.info())
 	print(data.describe())
 	
+	# Remove duplicate values
 	print('>> Before drop duplicate values: ')
 	print(f'>> Data shape: {data.shape}')
 	print(data.info())
@@ -22,5 +29,21 @@ def main():
 	print(data.info())
 	print(data.describe())
 	
+	# Remove outliers
+	Q1 = data.quantile(0.25)
+	Q3 = data.quantile(0.75)
+	IQR = Q3 - Q1
+	
+	print('>> Before drop outliers: ')
+	print(f'>> Data shape: {data.shape}')
+	print(data.info())
+	print(data.describe())
+
+	data = data[~((data < (Q1 - 1.5 * IQR)) |(data > (Q3 + 1.5 * IQR))).any(axis=1)]
+	
+	print('>> After drop outliers: ')
+	print(f'>> Data shape: {data.shape}')
+	print(data.info())
+	print(data.describe())
 if __name__ == '__main__':
 	main()
