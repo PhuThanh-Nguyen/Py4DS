2c6823c2ee32c7eaec40d12b1cf1a46bb3fff75a
ThanhNguyen-work <phuthanh.nguyen.work@gmail.com>
2020-11-26 09:16:22 +0700

    Implement correlation and statistical filter method and create few test cases

--- Lab 06/Bài tập 01/BaiTap01.py
+++ Lab 06/Bài tập 01/BaiTap01.py
@@ -6,12 +6,15 @@
 technologies.
 	2. Giving examples to demonstrate your function works
 '''
+import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score
-from sklearn.feature_selection import VarianceThreshold
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import VarianceThreshold, mutual_info_classif, SelectKBest, chi2
+from sklearn.decomposition import PCA
 
-def Constant_Features(X_train, X_test, threshold = 0):
-  """
+def ConstantFeatures(X_train, X_test, threshold = 0):
+  '''
 	Removing Constant Features using Variance Threshold
 	
 	Parameters:
@@ -22,7 +25,7 @@ def Constant_Features(X_train, X_test, threshold = 0):
 	
 	Returns: 
 		train data, test data after applying filter methods
-  """
+  '''
   # Create the VarianceThreshold object.
   vs_constant = VarianceThreshold(threshold)
 
@@ -47,7 +50,7 @@ def Constant_Features(X_train, X_test, threshold = 0):
   X_train = X_train.drop(labels = all_constant_columns, axis = 1)
   X_test = X_test.drop(labels = all_constant_columns, axis = 1)
   
-  return x_train, x_test
+  return X_train, X_test
 
 def quasiConstantFeatures(X_train, X_test, thres = 0.98):
 	
@@ -100,6 +103,80 @@ def duplicateFeatures(X_train, X_test):
 	X_test = X_test.drop(labels = duplicated_columns, axis = 1)
 	return X_train, X_test
 
+def correlationFilterMethod(X_train, X_test, thres = 0.8):
+	'''
+		Perform Correlation filter method
+		Parameters:
+			X_train, X_test: DataFrame objects
+				Training and testing data
+			thres: float, default: 0.8
+				Threshold of absolute value of correlation, must between 0 and 1
+		Returns:
+			X_train, X_test after applying correlation filter method
+	'''
+	# Creating set to hold the correlated features
+	corr_features = set()
+
+	# Create the correlation matrix (default to pearson)
+	corr_matrix = X_train.corr()
+
+	for i in range(len(corr_matrix.columns)):
+		for j in range(i):
+			if abs(corr_matrix.iloc[i, j]) > thres:
+				colname = corr_matrix.columns[i]
+				corr_features.add(colname)
+				
+	X_train = X_train.drop(labels = corr_features, axis = 1)
+	X_test = X_test.drop(labels = corr_features, axis = 1)
+	return X_train, X_test
+
+def mutualInfomation(numFeatures, X_train, y_train, X_test):
+
+	'''
+		Perform Mutual information filter method
+		Parameters:
+			numFeatures: int
+				Number of features to retain after filter
+			X_train, X_test, y_train: DataFrame/Series objects
+				Training data and testing data
+		Returns:
+			Training data and testing data after applying mutual information
+	'''
+
+	# Get only the numerical features.
+	numerical_X_train = X_train[X_train.select_dtypes([np.number]).columns]
+
+	# Create the SelectKBest with the mutual info strategy.
+	selection = SelectKBest(mutual_info_classif, k = numFeatures).fit(numerical_x_train, y_train)
+
+	features = X_train.columns[selection.get_support()]
+	
+	return X_train[features], X_test[features]
+
+def chiSquareMethod(numFeatures, X_train, y_train, X_test):
+	'''
+		Perform Chi-Square method
+		Parameters:
+			numFeatures: int
+				Number of features to retain after filter
+			X_train, X_test, y_train: DataFrame/Series objects
+				Training data and testing data
+		Returns:
+			Training data and testing data after applying chisquare method
+
+	'''
+	# Apply the chi2 score on the data and target (target should be binary).  
+	selection = SelectKBest(chi2, k = numFeatures).fit(X_train, y_train)
+
+	features = X_train.columns[selection.get_support()]
+	return X_train[features], X_test[features]
+
+def PCAMethod(num_components, X_train):
+	pca = PCA(n_components = num_components)
+	pca.fit(X_train)
+	X_train = pca.transform(X_train)
+	return X_train
+
 def BackwardFeatureElimination(X_train, y_train, X_test, y_test, clf):
 	
 	'''
@@ -147,7 +224,7 @@ def BackwardFeatureElimination(X_train, y_train, X_test, y_test, clf):
 			return X_train[cols], X_test[col]
 		cols = temp
 
-def ForwardFeatureElimination(X_train, y_train, X_test, y_test):
+def ForwardFeatureElimination(X_train, y_train, X_test, y_test, clf):
 	
 	'''
 		Perform forward feature elimination
@@ -196,7 +273,81 @@ def ForwardFeatureElimination(X_train, y_train, X_test, y_test):
 	return X_train[cols], X_test[cols]
 
 def main():
-	pass
+	# ------------------- Test Costant Features function -------------------
+	# Create dummy data to test
+	data = pd.DataFrame({'a' : [1,1,1,1], 'b' : [1,2,3,4], 'c' : [2,1,3,4], 'y' : [0,0,1,1]})
+	print(data)
+	print(data.info())
+	print(data.describe())
 
-if __name__ '__main__':
+	# Split dummy data
+	X, y = data.drop('y', axis = 1), data['y']
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+	print('*' * 20)
+	print('>> Before apply constant features filter')
+	print('>> X_train:')
+	print(X_train)
+	print('>> X_test:')
+	print(X_test)
+	# X_train_new and X_test_new must drop column `a`
+	X_train_new, X_test_new = ConstantFeatures(X_train, X_test)
+	print('>> After apply constant features filter')
+	print('>> X_train:')
+	print(X_train_new)
+	print('>> X_test:')
+	print(X_test_new)
+
+	# ------------------- Test quasiConstantFeatures function -------------------
+	# Create dummy data to test
+	data = pd.DataFrame({'a' : [1,1,2,1], 'b' : [1,2,3,3], 'c' : [2,1,3,4], 'y' : [0,0,1,1]})
+	print(data)
+	print(data.info())
+	print(data.describe())
+
+	# Split dummy data
+	X, y = data.drop('y', axis = 1), data['y']
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+	print('*' * 20)
+	print('>> Before apply Quasi-constant features filter')
+	print('>> X_train:')
+	print(X_train)
+	print('>> X_test:')
+	print(X_test)
+	# X_train_new and X_test_new must drop column `a`
+	X_train_new, X_test_new = quasiConstantFeatures(X_train, X_test, thres = 0.8)
+	print('>> After apply Quasi-constant features filter')
+	print('>> X_train:')
+	print(X_train_new)
+	print('>> X_test:')
+	print(X_test_new)
+
+	# ------------------- Test correlationFilterMethod function -------------------
+	rnd = np.random.RandomState(0)
+	# Create dummy data to test
+	data = pd.DataFrame({'a' : rnd.randn(10), 'b' : rnd.randn(10),\
+						 'c' : rnd.randn(10), 'd' : rnd.randn(10),\
+						 'y' : rnd.binomial(1, 0.4, size = 10)})
+	data['e'] = 2 * data['a'] + 1
+	data['f'] = data['b'] + 2 * data['a']
+	# Split data
+	X, y = data.drop('y', axis = 1), data['y']
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = rnd)
+
+	print('*' * 20)
+	print('>> Before apply correlation filter method')
+	print('>> X_train:')
+	print(X_train)
+	print('>> X_test:')
+	print(X_test)
+	# X_train_new and X_test_new must drop column `e` and `f`
+	X_train_new, X_test_new = correlationFilterMethod(X_train, X_test, thres = 0.8)
+	print('>> After apply correlation filter method')
+	print('>> X_train:')
+	print(X_train_new)
+	print('>> X_test:')
+	print(X_test_new)
+
+if __name__ == '__main__':
 	main()
